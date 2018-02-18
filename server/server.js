ea5cf8d02a6f3fda2bf3234e7da63409c6fa0461
Casey Heinemeyer <cheinemeyer@gmail.com>
2018-02-18 11:33:07 -0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+//Local files
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -23,6 +24,15 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 })
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
+
 
 
 
